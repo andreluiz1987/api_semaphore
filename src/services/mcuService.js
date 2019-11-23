0d53a9c7fce67d48b2eea94604627efb59e4ed82
andreluiz1987 <andre.coelho@hotmart.com>
2019-11-23 14:36:25 -0300

    test

--- src/services/mcuService.js
+++ src/services/mcuService.js
@@ -1,5 +1,8 @@
 'use scrict'
-const axios = require('axios');
+const axios = require('axios');var mqtt = require('mqtt');
+
+const mqtt_url = process.env.CLOUDMQTT_URL || 'mqtt://meclipav:g2ydzGLEva3M@tailor.cloudmqtt.com:15860';
+const topic = process.env.CLOUDMQTT_TOPIC || 'esp/test';
 
 exports.buildSuccess = async (req, res, next) => {
     let url = "http://192.168.0.4/text?data=success";
@@ -21,22 +24,31 @@ exports.buildSuccess = async (req, res, next) => {
 };
 
 exports.buildRunning = async (req, res, next) => {
-    let url = "http://192.168.0.4/text?data=running";
-    return axios.post(url)
-        .then(response => {
-            let code = response.status;
-            if (code == 200) {
-                res.status(200).send({
-                    message: response.data
-                });
-            }
-        })
-        .catch(error => {
-            //console.log(error);
-            res.status(400).send({
-                message: "Falha ao registrar build"
-            });
+
+    const client = mqtt.connect(mqtt_url);
+
+    client.on('connect', function() {
+        client.publish(topic, "running", function() {
+            res.writeHead(204, { 'Connection': 'keep-alive' });
+            res.end();
         });
+    });
+    // let url = "http://192.168.0.4/text?data=running";
+    // return axios.post(url)
+    //     .then(response => {
+    //         let code = response.status;
+    //         if (code == 200) {
+    //             res.status(200).send({
+    //                 message: response.data
+    //             });
+    //         }
+    //     })
+    //     .catch(error => {
+    //         //console.log(error);
+    //         res.status(400).send({
+    //             message: "Falha ao registrar build"
+    //         });
+    //     });
 };
 
 exports.buildFailed = async (req, res, next) => {
