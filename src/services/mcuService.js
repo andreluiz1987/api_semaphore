6a6501ef5e6352c4777253e289d1335a2e2c05c8
andreluiz1987 <andre.coelho@hotmart.com>
2019-11-23 15:18:51 -0300

    changes

--- src/services/mcuService.js
+++ src/services/mcuService.js
@@ -3,69 +3,31 @@ const axios = require('axios');var mqtt = require('mqtt');
 
 const mqtt_url = process.env.CLOUDMQTT_URL || 'mqtt://meclipav:g2ydzGLEva3M@tailor.cloudmqtt.com:15860';
 const topic = process.env.CLOUDMQTT_TOPIC || 'esp/test';
+const client = mqtt.connect(mqtt_url);
 
 exports.buildSuccess = async (req, res, next) => {
-    let url = "http://192.168.0.4/text?data=success";
-    return axios.post(url)
-        .then(response => {
-            let code = response.status;
-            if (code == 200) {
-                res.status(200).send({
-                    message: response.data
-                });
-            }
-        })
-        .catch(error => {
-            //console.log(error);
-            res.status(400).send({
-                message: "Falha ao registrar build"
-            });
+    client.on('connect', function() {
+        client.publish(topic, "success", function() {
+            res.writeHead(204, { 'Connection': 'keep-alive' });
+            res.end();
         });
+    });
 };
 
 exports.buildRunning = async (req, res, next) => {
-
-    const client = mqtt.connect(mqtt_url);
-
     client.on('connect', function() {
         client.publish(topic, "running", function() {
             res.writeHead(204, { 'Connection': 'keep-alive' });
             res.end();
         });
     });
-    // let url = "http://192.168.0.4/text?data=running";
-    // return axios.post(url)
-    //     .then(response => {
-    //         let code = response.status;
-    //         if (code == 200) {
-    //             res.status(200).send({
-    //                 message: response.data
-    //             });
-    //         }
-    //     })
-    //     .catch(error => {
-    //         //console.log(error);
-    //         res.status(400).send({
-    //             message: "Falha ao registrar build"
-    //         });
-    //     });
 };
 
 exports.buildFailed = async (req, res, next) => {
-    let url = "http://192.168.0.4/text?data=failed";
-    return axios.post(url)
-        .then(response => {
-            let code = response.status;
-            if (code == 200) {
-                res.status(200).send({
-                    message: response.data
-                });
-            }
-        })
-        .catch(error => {
-            //console.log(error);
-            res.status(400).send({
-                message: "Falha ao registrar build"
-            });
+    client.on('connect', function() {
+        client.publish(topic, "failed", function() {
+            res.writeHead(204, { 'Connection': 'keep-alive' });
+            res.end();
         });
+    });
 };
